0a0bb2d90374d314aa170f72d499ec5bfe3d161a
simabui <sima_sima@live.ru>
2020-04-20 14:22:45 +0300

    Updates

--- precache-manifest.e4c0d88d5e8c95d21dfd63189941f0e8.js
+++ precache-manifest.e4c0d88d5e8c95d21dfd63189941f0e8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c52e83bdd61a082b05bf",
+    "revision": "1364647debfc3cc9a6ce",
     "url": "/phonebook/static/css/main.3f890244.chunk.css"
   },
   {
-    "revision": "c52e83bdd61a082b05bf",
-    "url": "/phonebook/static/js/main.bbffbd33.chunk.js"
+    "revision": "1364647debfc3cc9a6ce",
+    "url": "/phonebook/static/js/main.e0e127f3.chunk.js"
   },
   {
     "revision": "1e8149b20a22033e7ae8",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/phonebook/static/js/2.3847d68e.chunk.js"
   },
   {
-    "revision": "36136779aae41c46cb4ca8ef7ab4c3bf",
+    "revision": "6a1297578dd8555b3e1e2322539f5ed3",
     "url": "/phonebook/index.html"
   }
 ];
